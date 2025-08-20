ab9dfd60d077524ad4eb4be8f36956066e8b2887
Team Sahasra <TeamSahasra@outlook.com>
2025-08-20 10:01:14 +0530

    Added Base Controller file

---
+++ src/SMS.UI/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMS.UI.Controllers
+{
+    public class BaseController : Controller
+    {
+        public DateTime GetDateTimeNow() 
+        { 
+            return DateTime.UtcNow; 
+        }
+
+        public IActionResult NotFound() 
+        {
+            return View();
+        }
+
+        public IActionResult Exception()
+        {
+            return View();
+        }
+    }
+}
