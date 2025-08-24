debd4c3e9db3490c45e12f9795fbf0a424e7f2f7
Team Sahasra <TeamSahasra@outlook.com>
2025-08-24 09:26:41 +0530

    23-08-2025 class changes

---
+++ src/SMS.UI/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMS.UI.Controllers
+{
+    public class AccountController : BaseController
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
