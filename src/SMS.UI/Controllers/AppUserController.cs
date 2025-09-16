305b6cabd1260758444d5000d0e8fbddfafc59cd
Team Sahasra <TeamSahasra@outlook.com>
2025-09-16 10:00:57 +0530

    Added AppUser Controller

---
+++ src/SMS.UI/Controllers/AppUserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMS.UI.Controllers
+{
+    public class AppUserController : BaseController
+    {
+        [HttpGet]
+        public IActionResult UserList()
+        {
+            ViewData["PageName"] = "Application Users";
+            return View();
+        }
+    }
+}
